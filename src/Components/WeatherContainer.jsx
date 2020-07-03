0b169ce3a115b7a92394411e70dbde2e061f23c0
MPiotrowska <monikapiotrowska27@gmail.com>
2020-07-03 22:33:27 +0200

    add logic

--- src/Components/WeatherContainer.jsx
+++ src/Components/WeatherContainer.jsx
@@ -1,28 +1,108 @@
 import React from "react";
+import humiditySVG from "../Images/humidity.svg";
+import sunSVG from "../Images/sun.svg";
+import windSVG from "../Images/wind.svg";
+import thermometerSVG from "../Images/thermometer.svg";
+import { kelvinToCelsius, meterToHour, convertToKm } from '../helpers/converters';
 
-function Column() {
+
+const getSvg = (text) => {
+  switch (text) {
+    case "Humidity":
+      return windSVG;
+    case "Visibility":
+      return thermometerSVG;
+    case "Description":
+      return sunSVG;
+    case "Wind":
+      return windSVG;
+  }
+};
+
+function Column(props) {
+  console.log(props.icon);
+
+  const hasIcon = props.icon;
+
   return (
-    <div className="columnContainer">
-      <div>
-        <img src="" alt="" />
-        <span>rain</span>
-        <p>40%</p>
+    <article className="weatherParameters smallerBox">
+      <div className="svg-container">
+        {hasIcon ? (
+          <img
+            src={`https://openweathermap.org/img/wn/${props.icon}@2x.png`}
+            width="25"
+            height="25"
+          />
+        ) : (
+          <img
+            class="svg"
+            src={getSvg(props.text)}
+            alt=""
+            width="20"
+            height="20"
+          />
+        )}
+        <span>{props.text === 'Description' && props.info ? props.info : props.text}</span>
       </div>
-    </div>
+      {props.text !== 'Description' && props.info && <p className='info'>{props.info}</p>}
+    </article>
   );
 }
 
-function WeatherContainer() {
+function WeatherContainer(props) {
+  const description =
+    (props &&
+      props.weatherInfo &&
+      props.weatherInfo.weather &&
+      props.weatherInfo.weather[0] &&
+      props.weatherInfo.weather[0].description) ||
+    "";
+
+  const icon =
+    (props &&
+      props.weatherInfo &&
+      props.weatherInfo.weather &&
+      props.weatherInfo.weather[0] &&
+      props.weatherInfo.weather[0].icon) ||
+    "";
+
+  const windSpeed = props?.weatherInfo?.wind?.speed ?? "";
+  const humidity = props?.weatherInfo?.main?.humidity
+    ? `${props.weatherInfo.main.humidity}%`
+    : "";
+
+  const visibility = props?.weatherInfo?.visibility ?? "";
+
+  const temperature = props?.weatherInfo?.main?.temp ?? "";
+
+  const feelsLike = props?.weatherInfo?.main?.feels_like
+
+  const tempToCelsius = temperature ? `${kelvinToCelsius(temperature)}°C` : '0°C'
+  const tempToCelsiusFeels = feelsLike ? `${kelvinToCelsius(feelsLike)}°C` : ''
+
+  const windToKm = windSpeed ? `${meterToHour(windSpeed)} km/h`: ''
+
+  const visibilityToKm =visibility ? `${convertToKm(visibility)} km` : ''
+
   return (
     <section className="weatherContainer">
-      blanla
       <div className="weatherContainer__inner">
         <div className="parametersBox">
-          <article className="weatherParameters mainBox">rgd</article>
-          <article className="weatherParameters smallerBox">rgd</article>
-          <article className="weatherParameters smallerBox">dgs</article>
-          <article className="weatherParameters smallerBox">dbb</article>
-          <article className="weatherParameters smallerBox">dxb</article>
+          <article className="weatherParameters mainBox">
+            <div>
+              <h2>{tempToCelsius}</h2>
+              {props?.weatherInfo?.main?.feels_like && (
+                <>
+                  <span>Feels like </span>
+                  <span>{tempToCelsiusFeels}</span>
+                </>
+              )}
+            </div>
+          </article>
+          <Column icon={icon} info={description} text="Description" />
+          <Column info={windToKm} text="Wind" />
+          <Column info={humidity} text="Humidity" />
+          <Column info={visibilityToKm} text="Visibility" />
         </div>
       </div>
     </section>
