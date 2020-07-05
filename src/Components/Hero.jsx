5f3ff730969512c0e9021937f44d3b3139498ddc
MPiotrowska <monikapiotrowska27@gmail.com>
2020-07-05 17:34:15 +0200

    add city name and date to hero

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,10 +1,14 @@
 import React from "react";
-import img from "../Images/md.jpg";
+import img from "../Images/fields-md.jpg";
 
-function Hero() {
+function Hero(props) {
   return (
     <div className="hero">
       <img src={img} alt="" />
+      <div className="city-date-container">
+        <p className="date">{props.date}</p>
+        <h2 className="city-name">{props.city}</h2>
+      </div>
     </div>
   );
 }
